5182f992c4a2403be100979e90dbf0201e8f4f8e
rori <rodte1234@gmail.com>
2025-05-27 17:51:49 -0600

    Fix: agregar componente FirstUsee

--- Frontend-Casino/src/pages/Menu/menu.jsx
+++ Frontend-Casino/src/pages/Menu/menu.jsx
@@ -7,7 +7,7 @@ const CasinoHero = () => {
       <div className="casino-hero-content">
         <h1>Welcome to <span className="highlight"> Colonial Casino </span></h1>
         <p>Where luck and excitement never stop. Play, win, and enjoy!</p>
-        <button className="play-button">Play Now!</button>
+        <button className="play-button">¡Play Now!</button>
       </div>
       <div className="casino-cards">
         <div className="card spade">♠</div>
