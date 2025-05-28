87383f1a716a23e50295d16893e09991d69a5c0d
Exequiel <exequiel.miranda245@gmail.com>
2025-05-28 11:04:19 -0600

    ppp

--- backend/app.js
+++ backend/app.js
@@ -15,7 +15,7 @@ app.use((req, res, next) => {
 
 app.use(
   cors({
-    origin: "https://remedial-casino-mu.vercel.app/",
+    origin: "https://remedial-casino-mu.vercel.app",
     credentials: true,
   })
 );
